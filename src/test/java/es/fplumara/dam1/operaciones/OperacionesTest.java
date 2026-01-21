d86a71f4783a8bd2793b37a9352c1ce5ad7e7c30
PatriciaOM23 <patricia.olmedo.majano23@gmail.com>
2026-01-21 12:20:12 +0100

    OperacionesTest actividad2, test calcular la media

--- src/test/java/es/fplumara/dam1/operaciones/OperacionesTest.java
+++ src/test/java/es/fplumara/dam1/operaciones/OperacionesTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -37,4 +39,10 @@ public class OperacionesTest {
             Operaciones.calificacion(notaNoValida);
         });
     }
+
+
+    //@ParameterizedTest
+   // @MethodSource();
+    //@DisplayName("Comprobar que hace la media")
+  //  void comprobarMedia(){}
 }
