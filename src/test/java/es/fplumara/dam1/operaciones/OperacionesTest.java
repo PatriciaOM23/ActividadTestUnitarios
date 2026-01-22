1f52c03bedaa90166436e13dfa2a6eb297657956
PatriciaOM23 <patricia.olmedo.majano23@gmail.com>
2026-01-22 08:31:35 +0100

    Me falta el ParametrizedTest y el MethodSource pero está hecho hasta el Mockito

--- src/test/java/es/fplumara/dam1/operaciones/OperacionesTest.java
+++ src/test/java/es/fplumara/dam1/operaciones/OperacionesTest.java
@@ -2,16 +2,16 @@ package es.fplumara.dam1.operaciones;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OperacionesTest {
     @ParameterizedTest
-    @CsvSource ({
+    @CsvSource({
             "0.0, 'INSUFICIENTE'",
             "4.99, 'INSUFICIENTE'",
             "5.0, 'APROBADO'",
@@ -22,19 +22,19 @@ public class OperacionesTest {
             "10.0, 'SOBRESALIENTE'"
     })
     @DisplayName("Comprueba si da el texto correcto al recibir una nota dentro de su rango")
-     void comprobarNotas(double nota, String esperado){
+    void comprobarNotas(double nota, String esperado) {
         String resultado = Operaciones.calificacion(nota);
-        assertEquals(esperado,resultado);
+        assertEquals(esperado, resultado);
     }
 
     @ParameterizedTest
-    @CsvSource ({
+    @CsvSource({
             "-0.01",
             "10.01"
     })
 
     @DisplayName("Comprueba si lanza la excepción al poner una nota que no entra en el rango 0-10")
-    void comprobarNotaNoValida(double notaNoValida){
+    void comprobarNotaNoValida(double notaNoValida) {
         IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> {
             Operaciones.calificacion(notaNoValida);
         });
@@ -42,7 +42,25 @@ public class OperacionesTest {
 
 
     //@ParameterizedTest
-   // @MethodSource();
+    // @MethodSource();
     //@DisplayName("Comprobar que hace la media")
-  //  void comprobarMedia(){}
+    //  void comprobarMedia(){}
+
+    @Test
+    @DisplayName("Comprobar el método media")
+    void testMediasConAssertAll() {
+        assertAll(
+                () -> assertEquals(6.0, Operaciones.media(5, 7)),
+                () -> assertEquals(10.0, Operaciones.media(10)),
+                () -> assertEquals(0.0, Operaciones.media(0, 0, 0))
+        );
+    }
+
+    @Test
+    @DisplayName("Comrpobar IllegalArgumentException cuando llaman sin notas o se pasa null")
+    void comprobarDatosNulos() {
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> {
+            Operaciones.media(null);
+        });
+    }
 }
