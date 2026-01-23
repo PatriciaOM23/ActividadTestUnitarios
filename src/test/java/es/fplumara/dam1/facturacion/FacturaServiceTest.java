4de7ad98020f6f5eb49822c46eaf8ef492672500
PatriciaOM23 <patricia.olmedo.majano23@gmail.com>
2026-01-23 11:47:44 +0100

    FacturaServiceTest hecho

--- src/test/java/es/fplumara/dam1/facturacion/FacturaServiceTest.java
+++ src/test/java/es/fplumara/dam1/facturacion/FacturaServiceTest.java
@@ -15,36 +15,33 @@ public class FacturaServiceTest {
     @ExtendWith(MockitoExtension.class)
 
     @Mock
-    private FacturaService facturaService;
-    @Mock
     private Calculadora calculadora;
 
-
-
+    private FacturaService facturaService;
 
     @BeforeEach
     public void setUp() {
-        facturaService = new FacturaService(calculadora);
+      facturaService = new FacturaService(calculadora);
     }
+
     @Test
     @DisplayName("Que totalConIva(100) devuelve 121 ")
-    public void comprobarTotalConIva(){
+    void comprobarTotalConIva(){
     //DEVUELVE 121 -- COMPORTAMIENTO
         when(calculadora.sumar(100, 21)).thenReturn(121);
+        when(calculadora.sumar(0,21)).thenReturn(21);
+
+
         int resultadoEsperado = facturaService.totalConIva(100);
+        int base2 = facturaService.totalConIva(0);
         assertEquals(121,resultadoEsperado);
-        verify(calculadora, times(1)).sumar(100, 21);
-
-      // DEVUELVE 21
-         when(calculadora.sumar(0,21)).thenReturn(21);
-          int base2 = facturaService.totalConIva(0);
-         assertEquals(21,calculadora.sumar(0,21));
-         verify(calculadora, times(2)).sumar(0, 21);
+        assertEquals(21,base2);
 
 
+        verify(calculadora).sumar(100, 21);
+        verify(calculadora).sumar(0, 21);
 
-
-        //verifyNoInteractions(calculadora);
+        verifyNoMoreInteractions(calculadora);
     }
 
 
